8f49efe4a06509639eb44c7adb96a09db00e3392
FannickZINSOU <adeniyizinsou@gmail.com>
2026-05-21 13:45:41 -0600

    fix verify token

--- server.js
+++ server.js
@@ -92,7 +92,7 @@ app.get("/webhook", (req, res) => {
   const mode = req.query["hub.mode"];
   const token = req.query["hub.verify_token"];
   const challenge = req.query["hub.challenge"];
-  if (mode === "subscribe" && token === process.env.VERIFY_TOKEN) {
+ if (mode === "subscribe" && token === (process.env.VERIFY_TOKEN || "FannickAgent2024")) {
     res.status(200).send(challenge);
   } else {
     res.sendStatus(403);
